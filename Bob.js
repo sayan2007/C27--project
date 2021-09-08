8a1f6044aa21a5b0ddaca9bb37820383bdbd3adf
sayan2007 <84729267+sayan2007@users.noreply.github.com>
2021-09-08 20:39:56 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,31 @@
+class Bob {
+
+constructor(x,y){
+    var options = {
+        isStatic:false,
+        restitution:1,
+        friction:0,
+        density: 7.8,
+    }
+
+   this.body = Bodies.circle(this.x,this.y,(this.r)/2,options);
+   World.add(world, this.body);
+
+   console.log(this.body);
+
+
+}
+
+
+display(){
+Push();
+pop();
+}
+
+
+
+
+
+
+}
+
